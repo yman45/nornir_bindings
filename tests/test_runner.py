9cd04017f2dfeb66f0fd6507fa808bdda7bb5c30
Yakov Sh <yman@protonmail.ch>
2018-12-10 02:26:30 +0300

    added add_to_inventory runner function

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,5 +1,6 @@
 import pytest
 from utils.runner import is_in_inventory, add_to_inventory
+from utils.runner import get_inventory_groups, NoGroupsHost
 
 
 @pytest.fixture
@@ -13,8 +14,7 @@ num_workers: 10
 inventory: nornir.plugins.inventory.simple.SimpleInventory
 SimpleInventory:
   host_file: "{}"
-  group_file: "{}"
-    '''.format(inv_hosts, inv_groups)
+  group_file: "{}"'''.format(inv_hosts, inv_groups)
     hosts = '''
 ---
 cisco-dc1:
@@ -26,8 +26,7 @@ huawei-dc2:
   nornir_host: 10.2.2.2
   groups:
     - dc_2
-    - huawei_tors
-    '''
+    - huawei_tors'''
     groups = '''
 ---
 defaults:
@@ -50,8 +49,7 @@ huawei_tors:
     - tors
   vendor: huawei
   lineup: ce
-  nornir_nos: huawei_vrpv8
-    '''
+  nornir_nos: huawei_vrpv8'''
     conf.write_text(test_config)
     inv_hosts.write_text(hosts)
     inv_groups.write_text(groups)
@@ -64,10 +62,25 @@ def test_is_in_inventory(tmp_inventory_files):
     assert is_in_inventory(config, 'huawei-dc2') is True
 
 
+def test_get_inventory_groups(tmp_inventory_files):
+    config = tmp_inventory_files
+    assert 'cisco_tors' in get_inventory_groups(config)
+    assert 'dc_2' in get_inventory_groups(config)
+
+
 def test_add_to_inventory(tmp_inventory_files):
     config = tmp_inventory_files
-    assert add_to_inventory(config, 'cisco-dc2', '10.3.3.3',
-                            'dc_2, cisco_tors') is True
+    add_to_inventory(config, 'cisco-dc2', '10.3.3.3',
+                     'dc_2, cisco_tors')
     assert is_in_inventory(config, 'cisco-dc2') is True
-    # assert add_to_inventory(config, 'cisco-dc1') is False
-    # check for parameters (group creation, nos, etc...)
+    with pytest.raises(ValueError):
+        add_to_inventory(config, 'cisco-dc3', '10.4.4.4.',
+                         'dc_3, cisco_tors')
+    assert add_to_inventory(
+            config, 'cisco-dc4', '10.5.5.5',
+            'dc_4, cisco_tors', no_such_group_ignore=True) is None
+    with pytest.raises(NoGroupsHost):
+        add_to_inventory(config, 'huawei-dc3', '10.6.6.6', 'dc_3, hawei_tors',
+                         no_such_group_ignore=True)
+    with pytest.raises(NoGroupsHost):
+        add_to_inventory(config, 'huawei-dc4', '10.7.7.7', '')
--- utils/runner.py
+++ utils/runner.py
@@ -1,7 +1,14 @@
 import click
 import ipaddress
 from yaml.scanner import ScannerError
+from ruamel.yaml import YAML
 from nornir.core import InitNornir
+
+
+class NoGroupsHost(Exception):
+    '''Exception to raise in a process of adding host into inventory if there
+    are no groups configured for that host.'''
+    pass
 
 
 @click.command()
@@ -22,12 +29,30 @@ def main(config, hostname):
             except socket.gaierror:
                 click.echo('Incorrect IP address, hostname or domain')
                 exit(1)
+        click.echo('Available groups: {}'.format(get_inventory_groups(config)))
         groups = click.prompt('''Enter groups separated by commas, spaces will
         be stripped, unknown groups ignored, new groups creation unsuppoted >
         ''')
         add_to_inventory(config, hostname, host_defnintion, groups)
     else:
         pass
+
+
+def get_inventory_groups(config):
+    '''Parse Nornir config file to find group inventory file and parse it next.
+    Grab all configured groups and return them.
+    Arguments:
+        * config - Nornir configuration file location
+    Returns:
+        view of dictionary keys, which represents groups defined in inventory
+    '''
+    yaml = YAML()
+    with open(config, 'r', encoding='utf-8') as config_file:
+        config_yaml = yaml.load(config_file)
+    group_inventory = config_yaml['SimpleInventory']['group_file']
+    with open(group_inventory, 'r', encoding='utf-8') as group_file:
+        groups = yaml.load(group_file)
+    return groups.keys()
 
 
 def is_in_inventory(config, hostname):
@@ -49,8 +74,47 @@ def is_in_inventory(config, hostname):
         return True
 
 
-def add_to_inventory(config, hostname, host_definition, groups):
-    pass
+def add_to_inventory(config, hostname, ip, groups, no_such_group_ignore=False):
+    '''Add host to Nornir inventory file. Raise NoGroupsHost exception if no
+    groups configured, or all of them not exist (in keys of
+    no_such_group_ignore flag is set).
+    Arguments:
+        * config - Nornir configuration file location
+        * hostname - host name, that will be added into inventory
+        * ip - host IP address (checking if it valid is out of scope of that
+            function)
+        * groups - string that represents groups this host will be added to;
+            group names must be separated by commas, spaces will be ignored
+        * no_such_group_ignore (default to False) - if True, silently drop
+            unexisted groups, otherwise raise ValueError
+    Returns nothing
+    '''
+    if not groups:
+        raise NoGroupsHost('No groups configured')
+    configured_groups = get_inventory_groups(config)
+    groups = [x.strip() for x in groups.split(',')]
+    cleaned_groups = []
+    # we are going to use this for stripping nonexisiting groups if
+    # no_such_group_ignore flag is set
+    for group in groups:
+        if group not in configured_groups:
+            if not no_such_group_ignore:
+                raise ValueError('Group {} is not configured'.format(group))
+            else:
+                continue
+        else:
+            cleaned_groups.append(group)
+    if len(cleaned_groups) == 0:
+        raise NoGroupsHost('All configured groups not exist')
+    host = {hostname: {"nornir_host": ip, "groups": cleaned_groups}}
+    yaml = YAML()
+    with open(config, 'r', encoding='utf-8') as config_file:
+        config_yaml = yaml.load(config_file)
+    host_inventory = config_yaml['SimpleInventory']['host_file']
+    yaml.indent(mapping=2, sequence=2, offset=2)
+    with open(host_inventory, 'a', encoding='utf-8') as host_file:
+        host_file.write('\n')
+        yaml.dump(host, host_file)
 
 
 if __name__ == '__main__':
